08280968d8d4f52c1dc60092c603f5715e5564a1
Michael Silva <msilva@princeton.com>
2022-04-05 15:19:19 -0400

    Adding docker-compose option and additional open connect args

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,20 @@
 
 set -e
 
-echo $VPN_PASSWORD | openconnect --user=$VPN_USER --passwd-on-stdin --csd-wrapper=/usr/libexec/openconnect/csd-wrapper.sh --script-tun --script "ocproxy -g -k 60 -D 9052" --os=linux-64 $VPN_HOST
+args="--user=$VPN_USER --passwd-on-stdin"
+if ! [[ -z $AUTH_GROUP ]]; then
+    args="$args --authgroup=$AUTH_GROUP"
+fi
+if ! [[ -z $SERVER_CERT_PIN ]]; then
+    args="$args --servercert=$SERVER_CERT_PIN"
+fi
+if ! [[ -z $FORM_ENTRY ]]; then
+    args="$args --form-entry=$FORM_ENTRY"
+fi
+if ! [[ -z $VERBOSITY ]]; then
+    args="$args -$VERBOSITY"
+fi
+
+echo $VPN_PASSWORD | openconnect $args --dump --csd-wrapper=/usr/libexec/openconnect/csd-wrapper.sh --script-tun --script "ocproxy -g -k 60 -D 9052" --os=linux-64 $VPN_HOST
 
 exec "$@"
